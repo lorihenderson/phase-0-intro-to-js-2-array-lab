0c083502b54f8a4e334449eb3b5f9a98a29f8c87
Lori Henderson <lorim95@gmail.com>
2022-05-18 22:51:50 -0400

    commit tree 2dbd011f6853fbe583b5ed7528d12c0423465b73

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name);
+}
+
+function appendCat(name) {
+    const newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats;
+}
+
+function removeLastCat() {
+    const newCats = [...cats];
+    return newCats.slice(0, newCats.length - 1);
+}
+
+function removeFirstCat() {
+    const newCats = [...cats];
+    return newCats.slice(newCats.length - 2);
+}
